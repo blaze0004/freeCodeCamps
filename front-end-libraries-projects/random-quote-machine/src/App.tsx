e3a66a4cd42b9a5dc8dd535fbac074540881d6ed
Haris Tyagi <tyagiharis370@gmail.com>
2021-08-29 22:01:18 +0530

    Random Quotes Machine Finished

--- front-end-libraries-projects/random-quote-machine/src/App.tsx
+++ front-end-libraries-projects/random-quote-machine/src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Quote from "./component/Quote";
 
-function App() {
+const quotesPath =
+  process.env.REACT_APP_QUOTE_PATH ??
+  "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+const getRandomColor = () => {
+  const r = Math.random() * 255;
+  const b = Math.random() * 255;
+  const g = Math.random() * 255;
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const getRandomQuote = (quotes: [] = []) => {
+  if (quotes.length) {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    return randomQuote;
+  }
+  return { quote: "", author: "" };
+};
+
+const App = () => {
+  const [quote, setQuote] = useState<{
+    author: string;
+    quote: string;
+    color: string;
+  }>({
+    quote: "",
+    author: "",
+    color: "green",
+  });
+  const [quotes, setQuotes] = useState<any>([]);
+
+  useEffect(() => {
+    fetch(quotesPath)
+      .then((data) => data.json())
+      .then(({ quotes }) => {
+        setQuotes(quotes);
+        setRandomQuote(quotes);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const setRandomQuote = (quotesData: [] | null = null) => {
+    const randomColor = getRandomColor();
+    const { quote, author } = getRandomQuote(quotesData ?? quotes);
+    setQuote({
+      quote,
+      author,
+      color: randomColor,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: quote.color }}>
+      {quote.quote ? (
+        <Quote
+          author={quote.author}
+          quote={quote.quote}
+          color={quote.color}
+          onNext={() => setRandomQuote()}
+        />
+      ) : (
+        <div>loading...</div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ front-end-libraries-projects/random-quote-machine/src/component/Quote.tsx
@@ -0,0 +1,81 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
+import Button from "./Button";
+
+const Quote: React.FC<{
+  quote: string;
+  author: string;
+  color: string;
+  onNext: () => void;
+}> = (props) => {
+  const { quote, author, color, onNext } = props;
+
+  return (
+    <div
+      id="quote-box"
+      style={{
+        padding: 20,
+        backgroundColor: "white",
+        borderRadius: 8,
+        display: "flex",
+        flexDirection: "column",
+        width: "40vw",
+      }}
+    >
+      <div
+        id="text"
+        style={{
+          textAlign: "center",
+          color: color,
+          fontSize: 18,
+          fontWeight: 500,
+          fontFamily: "cursive",
+          marginBottom: 20,
+        }}
+      >
+        <q>{quote}</q>
+      </div>
+      <div
+        id="author"
+        style={{
+          textAlign: "right",
+          color: color,
+        }}
+      >
+        -- {author}
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          marginTop: 20,
+        }}
+      >
+        <div>
+          <div
+            style={{
+              padding: "2px 8px",
+              borderRadius: 8,
+              backgroundColor: color,
+            }}
+          >
+            <a
+              href={`https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${quote}`}
+              target="_blank"
+              id="tweet-quote"
+              style={{ color: "white", textDecoration: "none" }}
+            >
+              Tweet <FontAwesomeIcon icon={faShare} size="1x" color="white" />
+            </a>
+          </div>
+        </div>
+        <div id="new-quote">
+          <Button bgColor={color} onClick={onNext} text="New quote" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Quote;
